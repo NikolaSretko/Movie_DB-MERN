41fbf00ccd599c5fe20184ab97a986165d2f6511
NikolaSretko <nikola.sretkovic99@gmail.com>
2024-02-05 01:06:36 +0100

    getAllFavCtrl path

--- backend/src/controller/index.js
+++ backend/src/controller/index.js
@@ -5,7 +5,7 @@ const addMovieCtrl = require('./addMovieCtrl.js')
 const updateMovieCtrl = require('./updateMovieCtrl')
 const removeMovieCtrl = require('./removeMovieCtrl.js')
 const addFavoriteCtrl = require('./addFavoriteCtrl.js')
-const getAllFavoritesCtrl = require('getAllFavoritesCtrl')
+const getAllFavoritesCtrl = require('./getAllFavoritesCtrl.js')
 const removeFavoriteCtrl = require('./removeFavorietCtrl.js')
 
 module.exports = {
